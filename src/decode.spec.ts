05915a7ed542f270d1cf137ded0930110ebb3ad3
icepeng <typ0@naver.com>
2019-11-01 10:59:06 +0900

    add testset

--- src/decode.spec.ts
+++ src/decode.spec.ts
@@ -1,44 +1,10 @@
 import { decode } from './decode';
+import { TEST_DATA } from './testset';
 
 describe('Decode', () => {
-  it('should return an array of cards', () => {
-    expect(decode('CEBAIAIFB4WDANQIAEAQGDAUDAQSIJZUAIAQCBIFAEAQCBAA')).toStrictEqual([
-      { code: '01SI015', count: 3 },
-      { code: '01SI044', count: 3 },
-      { code: '01SI048', count: 3 },
-      { code: '01SI054', count: 3 },
-      { code: '01FR003', count: 3 },
-      { code: '01FR012', count: 3 },
-      { code: '01FR020', count: 3 },
-      { code: '01FR024', count: 3 },
-      { code: '01FR033', count: 3 },
-      { code: '01FR036', count: 3 },
-      { code: '01FR039', count: 3 },
-      { code: '01FR052', count: 3 },
-      { code: '01SI005', count: 2 },
-      { code: '01FR004', count: 2 },
-    ]);
-  });
-
-  it('should return an array of cards', () => {
-    expect(decode('CEBAEAIBAQTQMAIAAILSQLBNGUBACAIBFYDACAAHBEHR2IBLAEBACAIFAY')).toStrictEqual([
-      { code: '01FR004', count: 3 },
-      { code: '01FR039', count: 3 },
-      { code: '01DE002', count: 3 },
-      { code: '01DE023', count: 3 },
-      { code: '01DE040', count: 3 },
-      { code: '01DE044', count: 3 },
-      { code: '01DE045', count: 3 },
-      { code: '01DE053', count: 3 },
-      { code: '01FR046', count: 2 },
-      { code: '01DE007', count: 2 },
-      { code: '01DE009', count: 2 },
-      { code: '01DE015', count: 2 },
-      { code: '01DE029', count: 2 },
-      { code: '01DE032', count: 2 },
-      { code: '01DE043', count: 2 },
-      { code: '01FR005', count: 1 },
-      { code: '01FR006', count: 1 },
-    ]);
-  });
+  for (const { code, deck } of TEST_DATA) {
+    it('should return an array of cards', () => {
+      expect(decode(code)).toStrictEqual(deck);
+    });
+  }
 });
--- src/encode.spec.ts
+++ src/encode.spec.ts
@@ -1,48 +1,10 @@
 import { encode } from './encode';
+import { TEST_DATA } from './testset';
 
 describe('Encode', () => {
-  it('should return a deck code', () => {
-    expect(
-      encode([
-        { code: '01SI015', count: 3 },
-        { code: '01SI044', count: 3 },
-        { code: '01SI048', count: 3 },
-        { code: '01SI054', count: 3 },
-        { code: '01FR003', count: 3 },
-        { code: '01FR012', count: 3 },
-        { code: '01FR020', count: 3 },
-        { code: '01FR024', count: 3 },
-        { code: '01FR033', count: 3 },
-        { code: '01FR036', count: 3 },
-        { code: '01FR039', count: 3 },
-        { code: '01FR052', count: 3 },
-        { code: '01SI005', count: 2 },
-        { code: '01FR004', count: 2 },
-      ]),
-    ).toEqual('CEBAIAIFB4WDANQIAEAQGDAUDAQSIJZUAIAQCBIFAEAQCBAA');
-  });
-
-  it('should return a deck code', () => {
-    expect(
-      encode([
-        { code: '01FR004', count: 3 },
-        { code: '01FR039', count: 3 },
-        { code: '01DE002', count: 3 },
-        { code: '01DE023', count: 3 },
-        { code: '01DE040', count: 3 },
-        { code: '01DE044', count: 3 },
-        { code: '01DE045', count: 3 },
-        { code: '01DE053', count: 3 },
-        { code: '01FR046', count: 2 },
-        { code: '01DE007', count: 2 },
-        { code: '01DE009', count: 2 },
-        { code: '01DE015', count: 2 },
-        { code: '01DE029', count: 2 },
-        { code: '01DE032', count: 2 },
-        { code: '01DE043', count: 2 },
-        { code: '01FR005', count: 1 },
-        { code: '01FR006', count: 1 },
-      ]),
-    ).toEqual('CEBAEAIBAQTQMAIAAILSQLBNGUBACAIBFYDACAAHBEHR2IBLAEBACAIFAY');
-  });
+  for (const { code, deck } of TEST_DATA) {
+    it('should return a deck code', () => {
+      expect(encode(deck)).toEqual(code);
+    });
+  }
 });
